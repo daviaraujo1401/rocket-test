e8b7e4de26c8a1792f2cde4656bb3aded9447fcd
davi <davivasconcelos@cjr.org.br>
2019-07-03 22:39:23 -0300

    chore: altera rota

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-  res.json({ message: 'Ola' })
+app.get('', (req, res) => {
+  res.json({ message: 'Hello' })
 })
 
 app.listen(3000)
